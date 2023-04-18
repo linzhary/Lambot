f96eb449b529a7331657678c39d74e71f60c39d7
magic-lamb <magic-lamb@outlook.com>
2023-04-18 20:35:48 +0800

    :zap: Improve performance.

--- src/adapters/Lambot.Adapters.OneBot/OneBotEventMatcher.cs
+++ src/adapters/Lambot.Adapters.OneBot/OneBotEventMatcher.cs
@@ -58,9 +58,10 @@ internal class OneBotEventMatcher : IPluginMatcher
             {
                 await _bot.SendPrivateMessageAsync(privateEvt.UserId, message, privateEvt.GroupId);
             }
-        }        
+        }
     }
 
+    private readonly ConcurrentDictionary<MethodInfo, Delegate> _methodCache = new();
     private async Task<object> Invoke<TEvent>(PluginMatcherParameter parameter)
         where TEvent : LambotEvent
     {
@@ -71,63 +72,85 @@ internal class OneBotEventMatcher : IPluginMatcher
 
         _context.IsBreaked = parameter.TypeMatcher.Break;
 
-        var parameters = new List<object>();
-        foreach (var parameterInfo in parameter.MethodInfo.GetParameters())
+        var parameterValues = new List<object>();
+        var parameterInfos = parameter.MethodInfo.GetParameters();
+        foreach (var parameterInfo in parameterInfos)
         {
             if (parameterInfo.ParameterType.IsAssignableTo(typeof(Bot)))
             {
-                parameters.Add(_bot);
+                parameterValues.Add(_bot);
             }
             else if (parameterInfo.ParameterType.IsAssignableTo(typeof(LambotContext)))
             {
-                parameters.Add(_context);
+                parameterValues.Add(_context);
             }
             else if (parameterInfo.ParameterType.IsAssignableTo(typeof(TEvent)))
             {
-                parameters.Add(parameter.Event);
+                parameterValues.Add(parameter.Event);
             }
             else if (parameterInfo.ParameterType.IsValueType)
             {
-                parameters.Add(Activator.CreateInstance(parameterInfo.ParameterType));
+                parameterValues.Add(Activator.CreateInstance(parameterInfo.ParameterType));
             }
             else if (parameter.RuleMatcher is OnRegex regexMatcher)
             {
                 if (regexMatcher.MatchResult is null)
                 {
-                    parameters.Add(null);
+                    parameterValues.Add(null);
                 }
                 else if (parameterInfo.ParameterType.IsAssignableTo(typeof(Match)))
                 {
-                    parameters.Add(regexMatcher.MatchResult);
+                    parameterValues.Add(regexMatcher.MatchResult);
                 }
                 else if (parameterInfo.ParameterType.IsAssignableTo(typeof(Group[])))
                 {
-                    parameters.Add(regexMatcher.MatchResult.Groups.Cast<Group>().Skip(1).ToArray());
+                    parameterValues.Add(regexMatcher.MatchResult.Groups.Cast<Group>().Skip(1).ToArray());
                 }
                 else if (parameterInfo.ParameterType.IsAssignableTo(typeof(IEnumerable<Group>)))
                 {
-                    parameters.Add(regexMatcher.MatchResult.Groups.Cast<Group>().Skip(1));
+                    parameterValues.Add(regexMatcher.MatchResult.Groups.Cast<Group>().Skip(1));
                 }
                 else
                 {
-                    parameters.Add(null);
+                    parameterValues.Add(null);
                 }
             }
             else
             {
-                parameters.Add(null);
+                parameterValues.Add(null);
             }
         }
         try
         {
-            var result = parameter.MethodInfo.Invoke(parameter.PluginInstance, parameters.ToArray());
-            return await AsyncUtils.UnwrapAsyncResult(result);
+            return await _methodCache.GetOrAdd(parameter.MethodInfo, (_) =>
+            {
+                var instanceExpr = Expression.Parameter(parameter.MethodInfo.DeclaringType, "instance");
+                var parameterExprs = new List<ParameterExpression>();
+                for (var i = 0; i < parameter.MethodInfo.GetParameters().Count(); i++)
+                {
+                    parameterExprs.Add(Expression.Parameter(parameter.MethodInfo.GetParameters()[i].ParameterType, $"arg_{i}"));
+                }
+                var methodCallExpr = Expression.Call(instanceExpr, parameter.MethodInfo, parameterExprs);
+                return Expression.Lambda(methodCallExpr, new[] { instanceExpr }.Concat(parameterExprs).ToArray()).Compile();
+            })
+            .DynamicInvoke(new[] { parameter.PluginInstance }.Concat(parameterValues).ToArray())
+            .UnwrapAsyncResult();
         }
         catch (LambotContextException ex)
         {
             if (ex.IsFinish)
             {
                 return ex.Message;
+            }
+        }
+        catch (TargetInvocationException ex)
+        {
+            if (ex.InnerException is LambotContextException innerEx)
+            {
+                if (innerEx.IsFinish)
+                {
+                    return ex.Message;
+                }
             }
         }
         return null;
