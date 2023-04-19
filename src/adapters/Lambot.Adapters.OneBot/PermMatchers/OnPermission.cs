fa04249ea882a1e3a5a0421b3c3ab880c43d1f14
magic-lamb <magic-lamb@outlook.com>
2023-04-19 16:10:00 +0800

    :art: Improve structure / format of the code.

---
+++ src/adapters/Lambot.Adapters.OneBot/PermMatchers/OnPermission.cs
@@ -0,0 +1,49 @@
+﻿using Lambot.Core;
+using Lambot.Core.Plugin;
+
+namespace Lambot.Adapters.OneBot;
+
+public class OnPermission : PermMatcher
+{
+    /// <summary>
+    /// 用户ID，默认或传NULL允许所有
+    /// </summary>
+    public long[] Users { get; set; } = new long[0];
+
+    /// <summary>
+    /// 群聊ID，默认或传NULL允许所有
+    /// </summary>
+    public long[] Groups { get; set; } = new long[0];
+
+    /// <summary>
+    /// 群聊角色，默认或传NULL允许所有
+    /// </summary>
+    public GroupUserRole Role { get; set; } = GroupUserRole.Unknow;
+
+    public override bool Check(LambotEvent evt)
+    {
+        var messageEvent = evt as MessageEvent;
+        if (messageEvent is null) return false;
+        if (messageEvent is GroupMessageEvent groupEvent)
+        {
+            //群号过滤
+            if (Groups is not null && !Groups.Contains(groupEvent.GroupId))
+            {
+                return false;
+            }
+            //角色过滤
+            if (Role is not GroupUserRole.Unknow
+                && (Role & groupEvent.Sender.Role) == 0)
+            {
+                return false;
+            }
+        }
+
+        if (Users is not null && !Users.Contains(messageEvent.UserId))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
