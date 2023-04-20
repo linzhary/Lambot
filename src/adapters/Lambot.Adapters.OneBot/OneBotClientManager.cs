da3a4dab8624a267e6312345a0e593c707b1662b
magic-lamb <magic-lamb@outlook.com>
2023-04-20 16:28:51 +0800

    :recycle: Refactor code.

---
+++ src/adapters/Lambot.Adapters.OneBot/OneBotClientManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lambot.Adapters.OneBot;
+
+public class OneBotClientManager
+{
+    private readonly ConcurrentDictionary<long, OneBotClient> _clientMap = new();
+
+    internal void Add(OneBotClient client)
+    {
+        _clientMap.AddOrUpdate(client.UserId, client, (_, _) => client);
+    }
+
+    internal OneBotClient Get(long user_id)
+    {
+        return _clientMap.GetValueOrDefault(user_id, null);
+    }
+}
