f94189050fc3bfabf59b40167894b4c9fed1fb00
magic-lamb <magic-lamb@outlook.com>
2023-04-20 21:56:50 +0800

    :bug: Fix a bug.

--- src/Lambot.Template/Plugins/FastLearning/FastLearningDbContext.cs
+++ src/Lambot.Template/Plugins/FastLearning/FastLearningDbContext.cs
@@ -5,7 +5,7 @@ namespace Lambot.Template.Plugins.FastLearning;
 
 public class FastLearningDbContext : DbContext
 {
-    public DbSet<FastLearningRecord> Records { get; set; }
+    public DbSet<FastLearningRecord> Records => Set<FastLearningRecord>();
 
     public FastLearningDbContext(DbContextOptions<FastLearningDbContext> options) : base(options)
     {
